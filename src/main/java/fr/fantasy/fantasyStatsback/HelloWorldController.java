2847e171e0470a3f98cd10bea8c7959d21d11c5a
ATThompson <antoine.thompson@outlook.fr>
2023-07-14 21:48:30 +0200

    okok

--- src/main/java/fr/fantasy/fantasyStatsback/HelloWorldController.java
+++ src/main/java/fr/fantasy/fantasyStatsback/HelloWorldController.java
@@ -1,18 +1,13 @@
 package fr.fantasy.fantasyStatsback;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
-    @Value("${test.test}")
-    String test;
     @RequestMapping("/")
     public String hello()
     {
-        return "Hello "+test;
+        return "Hello Everyone";
     }
 }
